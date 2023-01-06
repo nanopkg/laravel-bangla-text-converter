bf09f09d70b374526a9f17dff025594201745161
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2023-01-06 12:05:14 +0600

    extra bug fix

--- src/Facades/LaravelBanglaTextConverter.php
+++ src/Facades/LaravelBanglaTextConverter.php
@@ -5,9 +5,9 @@ namespace Nanopkg\LaravelBanglaTextConverter\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * /**
- * @method static string toUnicode(string $string)
  * @method static string toBijoy(string $string)
+ *
+ * @see \Nanopkg\LaravelBanglaTextConverter\LaravelBanglaTextConverter
  */
 class LaravelBanglaTextConverter extends Facade
 {
