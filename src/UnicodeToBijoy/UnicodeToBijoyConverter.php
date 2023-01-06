82c19748d97ae62dbd82481d391e74bbe5de8e98
iqbalhasandev <iqbalhasandev@users.noreply.github.com>
2023-01-06 05:16:33 +0000

    Fix styling

--- src/UnicodeToBijoy/UnicodeToBijoyConverter.php
+++ src/UnicodeToBijoy/UnicodeToBijoyConverter.php
@@ -63,7 +63,7 @@ class UnicodeToBijoyConverter implements Converter
 
                 if ($count = mb_strlen($match[1])) {
                     $space = ' ';
-                    $key = $space . $key;
+                    $key = $space.$key;
                 }
 
                 $rule = KeyMapping::KAR_AROUND_CHAR_RULES[$key];
@@ -73,7 +73,7 @@ class UnicodeToBijoyConverter implements Converter
                 }
                 $rule = str_replace('<char>', $char, $rule);
 
-                return $space . $rule;
+                return $space.$rule;
             },
             $string
         );
@@ -97,12 +97,12 @@ class UnicodeToBijoyConverter implements Converter
             function ($match) {
                 // r($match);
                 $return = $match['kar'];
-                if (!empty($match['b'])) {
+                if (! empty($match['b'])) {
                     $return .= $match['b'];
                 }
                 $return .= $match['char'];
 
-                if (!empty($match['z'])) {
+                if (! empty($match['z'])) {
                     $return .= $match['z'];
                 }
 
